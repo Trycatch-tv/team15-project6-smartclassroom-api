a15bda5ed8bb934ed940cc9273cf670e0e7bb9bd
Joshua_Arfa <soondevjosh@gmail.com>
2023-04-09 15:44:08 -0600

    endpoints-deletAndeditDetailCourses

--- api/controllers/courses.js
+++ api/controllers/courses.js
@@ -13,3 +13,19 @@ export const createCourse = async (req, res) => {
     res.status(404).json(err)
   }
 }
+
+export const editCourseDetail = async (req, res) => {
+  try {
+    res.status(200).json({ message: 'editing course detail' })
+  } catch (err) {
+    res.status(404).json(err)
+  }
+}
+
+export const deleteCourse = async (req, res) => {
+  try {
+    res.status(200).json({ message: 'delete course' })
+  } catch (err) {
+    res.status(404).json(err)
+  }
+}
--- api/routes/courses.js
+++ api/routes/courses.js
@@ -1,9 +1,12 @@
 import { Router } from 'express'
-import { courseDetail, createCourse } from '../controllers/courses.js'
+import { courseDetail, createCourse, deleteCourse, editCourseDetail } from '../controllers/courses.js'
 
 const route = Router()
 
 route.post('/courses', createCourse)
 route.get('/courses/:id', courseDetail)
+
+route.patch('/courses/:id', editCourseDetail)
+route.delete('/courses/:id', deleteCourse)
 
 export default route
