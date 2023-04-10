63980357bda28d6c49d14198995e17bb759fd7ef
luisdav1 <prueba@gmail.com>
2023-04-10 13:10:32 -0500

    change app.js error message

--- courseController.mjs
+++
@@ -1,9 +0,0 @@
-export const getCourses = async (req, res) => {
-    try { 
-      //(código de estado HTTP 200)
-      res.status(200).json({message: 'showing the courses'});
-    } catch (error) {
-      // Si ocurre algún error, enviamos una respuesta con un mensaje de error y un código de estado HTTP 500 (Error interno del servidor)
-      res.status(500).json({ message: 'Failed to display courses' });
-    }
-  };
