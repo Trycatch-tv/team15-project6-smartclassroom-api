42f22e4ddce38f023b63a34431318fa7cbb6d1f6
luisdav1 <prueba@gmail.com>
2023-04-10 12:39:33 -0500

    Add getCourses and route to get the courses

--- api/routes/courses.js
+++ api/routes/courses.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { courseDetail, createCourse } from '../controllers/courses.js'
+import { courseDetail, createCourse, getCourses } from '../controllers/courses.js'
+
 
 const route = Router()
 
 route.post('/courses', createCourse)
 route.get('/courses/:id', courseDetail)
-
+route.get('/courses', getCourses);
 export default route
