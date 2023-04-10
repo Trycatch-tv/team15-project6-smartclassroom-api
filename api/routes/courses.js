8df84be71d30a821baa253522a93ca7640a08318
Joshua_Arfa <soondevjosh@gmail.com>
2023-04-10 14:42:50 -0600

    updating delete-editDetailCourse-endpoints

--- api/routes/courses.js
+++ api/routes/courses.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import { courseDetail, createCourse, getCourses } from '../controllers/courses.js'
-
+import { courseDetail, createCourse, deleteCourse, editCourseDetail, getCourses } from '../controllers/courses.js'
 
 const route = Router()
 
 route.post('/courses', createCourse)
 route.get('/courses/:id', courseDetail)
-route.get('/courses', getCourses);
+route.get('/courses', getCourses)
+route.put('/courses/:id', editCourseDetail)
+route.delete('/courses/:id', deleteCourse)
 export default route
