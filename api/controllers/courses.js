a68b2d0b9f43aefd1c8cbfadc6085e8f2ea685e6
luisdav1 <prueba@gmail.com>
2023-04-10 16:19:52 -0500

    fix errors

--- api/controllers/courses.js
+++ api/controllers/courses.js
@@ -14,11 +14,10 @@ export const createCourse = async (req, res) => {
   }
 }
 export const getCourses = async (req, res) => {
-  try { 
-    //(código de estado HTTP 200)
-    res.status(200).json({message: 'showing the courses'});
-  } catch (error) {
+  try {
+    res.status(200).json({ message: 'showing the courses' })
+  } catch (err) {
     // Si ocurre algún error, enviamos una respuesta con un mensaje de error y un código de estado HTTP 500 (Error interno del servidor)
-    res.status(500).json({ message: 'Failed to display courses' });
+    res.status(500).json({ message: 'Failed to display courses' })
   }
-};
+}
--- api/routes/courses.js
+++ api/routes/courses.js
@@ -1,10 +1,9 @@
 import { Router } from 'express'
 import { courseDetail, createCourse, getCourses } from '../controllers/courses.js'
 
-
 const route = Router()
 
 route.post('/courses', createCourse)
 route.get('/courses/:id', courseDetail)
-route.get('/courses', getCourses);
+route.get('/courses', getCourses)
 export default route
